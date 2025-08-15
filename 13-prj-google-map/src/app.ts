42259b43c5eeb24d50abfd9d1b7cd98f53190875
kims0uce <ksy_59@naver.com>
2025-08-15 23:11:42 +0900

    axios 호출 추가

--- 13-prj-google-map/src/app.ts
+++ 13-prj-google-map/src/app.ts
@@ -1,2 +1,29 @@
-// Code goes here!
-// 주소를 입력할 수 있는 간단한 웹 애플리케이션 만들기
+import axios from "axios";
+
+const form = document.querySelector("form")!; // null값이 아니라는 것을 ts에 알려줌
+const addressIntput = document.getElementById("address")! as HTMLInputElement; // value에 접근하기 위해 html element라는걸 명시함
+const GOOGLE_API_KEY = process.env.GOOGLE_API;
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault();
+  const enteredAddress = addressIntput.value;
+
+  // send this to Google api
+  // 서드파티 쓰기 싫으면 fetch 사용 가능
+  // ts 프로젝트에서 axios 사용해보기
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// submit 이벤트가 발생할 때 마다 함수 실행
+form.addEventListener("submit", searchAddressHandler);
--- 13-prj-google-map/webpack.config.js
+++ 13-prj-google-map/webpack.config.js
@@ -1,23 +1,35 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
+  mode: "development",
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
 };
